fe1a71b6825e0bfd43846683102a4bbf0fbf3263
Vanesuki <vanessamachucacano@gmail.com>
2020-08-22 13:17:23 +0200

    tercer commit

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -15,8 +15,21 @@ class Soldier:
 
 # Viking
 
-class Viking:
-    pass
+class Viking(Soldier):
+
+    def __init__(self, name, health, strength):
+        self.name = name
+        super().__init__(health, strength)
+        
+    def receiveDamage(self, damage):
+        super().receiveDamage(damage)
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 
 
